ecb3347bfa47723f3d18d05f161ce33a7c6e9e94
ozefreitas <jpsfreitas12@gmail.com>
2025-03-14 00:32:04 +0000

    [UPDATE] Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,28 @@
 import styles from "./header.module.css";
 import skipLogo from "../../assets/skip-logo.png";
 
-export default function Header({}) {
+interface HeaderProps {
+  isLogedIn: boolean
+}
+
+export default function Header({ isLogedIn }: HeaderProps) {
   return (
     <div className={styles.main_header_container}>
       <div className={styles.app_name}>
         <img src={skipLogo} alt="SKI-P Logo" className={styles.logo} />
         <a href="">Karate Score App</a>
       </div>
-      <div className={styles.dojos_actions}>
-        <button>Login</button>
-        <button>Criar Conta</button>
-      </div>
+      {isLogedIn ? (
+        <div className={styles.dojos_actions}>
+          <button className={styles.login_buttons}>Logout</button>
+          <button className={styles.login_buttons}>Perfil</button>
+        </div>
+      ) : (
+        <div className={styles.dojos_actions}>
+          <button className={styles.login_buttons}>Login</button>
+          <button className={styles.login_buttons}>Criar Conta</button>
+        </div>
+      )}
     </div>
   );
 }
