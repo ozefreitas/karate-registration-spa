b94b06cf3a5cbec49e4eb89dc3adb136fa5a975e
ozefreitas <jpsfreitas12@gmail.com>
2025-06-17 23:23:35 +0100

    [ADD] error pages

---
+++ src/pages/ErrorPages/UnAuthorizedPage.tsx
@@ -0,0 +1,18 @@
+import { Button, Grid, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+export default function UnAuthorizedPage() {
+  const navigate = useNavigate();
+  return (
+    <>
+      <Grid container size={10} justifyContent="center">
+        <Typography variant="h3">Acesso negado</Typography>
+      </Grid>
+      <Grid container justifyContent="center" sx={{ m: 2 }}>
+        <Button variant="contained" onClick={() => navigate("/")}>
+          Voltar
+        </Button>
+      </Grid>
+    </>
+  );
+}
