1b436839420e1d9928842052a0f3d09599c74639
ozefreitas <jpsfreitas12@gmail.com>
2025-06-22 13:42:05 +0100

    [ADD] team profile page

--- src/pages/TeamsPage/SingleTeamPage.tsx
+++ src/pages/TeamsPage/SingleTeamPage.tsx
@@ -1,3 +1,35 @@
+import {
+  Grid,
+  Stack,
+  Typography,
+  Button,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemButton,
+  Card,
+  CardHeader,
+  CardContent,
+} from "@mui/material";
+
 export default function SingleTeamPage() {
-  return <div></div>;
+  return (
+    <>
+      <Card sx={{ m: 2, mt: 0 }}>
+        <CardHeader
+          title={"Perfil da Equipa"}
+          sx={{
+            "& .MuiCardHeader-title": {
+              fontWeight: "bold",
+            },
+          }}
+        ></CardHeader>
+        <CardContent>
+          Nesta página poderá consultar toda a informação de uma Equipa, assim
+          como editar ou remover esta mesma Equipa.
+        </CardContent>
+      </Card>
+      
+    </>
+  );
 }
