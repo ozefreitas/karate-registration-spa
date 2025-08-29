0390d37d1c42ebc5bb9dc7bfc1c26e0f60c938ee
ozefreitas <jpsfreitas12@gmail.com>
2025-08-29 08:07:47 +0100

    [ADD] queries for token logic

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -132,3 +132,178 @@ export const useRemoveClub = () => {
     },
   });
 };
+
+const fetchTokenUsername = (token: string) => {
+  return axios.get(`http://127.0.0.1:8000/get_token_username/?token=${token}`);
+};
+
+export const useFetchTokenUsername = (token: string) => {
+  return useQuery({
+    queryKey: ["token_username", token],
+    queryFn: () => fetchTokenUsername(token),
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    retry: false,
+  });
+};
+
+const fetchToken = (username: string) => {
+  const token = localStorage.getItem("token");
+  return axios.get(
+    `http://127.0.0.1:8000/get_token_by_username/?username=${username}`,
+    {
+      headers: {
+        Authorization: `Token ${token}`,
+      },
+    }
+  );
+};
+
+export const useFetchToken = (username: string) => {
+  return useQuery({
+    queryKey: ["token_username", username],
+    queryFn: () => fetchToken(username),
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    retry: false,
+  });
+};
+
+const fetchRequestingAccounts = () => {
+  const token = localStorage.getItem("token");
+  return axios.get(`http://127.0.0.1:8000/request_acount/`, {
+    headers: {
+      Authorization: `Token ${token}`,
+    },
+  });
+};
+
+export const useFetchRequestingAccounts = () => {
+  return useQuery({
+    queryKey: ["request-acount"],
+    queryFn: fetchRequestingAccounts,
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+  });
+};
+
+const createRequestAcount = (data: any) => {
+  return axios.post(`http://127.0.0.1:8000/request_acount/`, data);
+};
+
+export const useCreateRequestAcount = () => {
+  const { enqueueSnackbar } = useSnackbar();
+
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({ data }: { data: any }) => createRequestAcount(data),
+    onSuccess: () => {
+      enqueueSnackbar("Pedido de conta enviado! Será contactado em breve", {
+        variant: "success",
+        anchorOrigin: {
+          vertical: "top",
+          horizontal: "center",
+        },
+        autoHideDuration: 5000,
+        preventDuplicate: true,
+      });
+      queryClient.invalidateQueries({ queryKey: ["request-acount"] });
+    },
+    onError: () => {
+      enqueueSnackbar("Um erro ocorreu! Tente novamente.", {
+        variant: "error",
+        anchorOrigin: {
+          vertical: "top",
+          horizontal: "center",
+        },
+        autoHideDuration: 5000,
+        preventDuplicate: true,
+      });
+    },
+  });
+};
+
+const removeRequestAcount = (requestAcountId: string) => {
+  const token = localStorage.getItem("token");
+  return axios.delete(
+    `http://127.0.0.1:8000/request_acount/${requestAcountId}/`,
+    {
+      headers: {
+        Authorization: `Token ${token}`,
+      },
+    }
+  );
+};
+
+export const useRemoveRequestAcount = () => {
+  const { enqueueSnackbar } = useSnackbar();
+
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: removeRequestAcount,
+    onSuccess: () => {
+      enqueueSnackbar("Pedido de conta rejeitado e removido.", {
+        variant: "success",
+        anchorOrigin: {
+          vertical: "top",
+          horizontal: "center",
+        },
+        autoHideDuration: 5000,
+        preventDuplicate: true,
+      });
+      queryClient.invalidateQueries({ queryKey: ["request-acount"] });
+    },
+    onError: () => {
+      enqueueSnackbar("Um erro ocorreu! Tente novamente.", {
+        variant: "error",
+        anchorOrigin: {
+          vertical: "top",
+          horizontal: "center",
+        },
+        autoHideDuration: 5000,
+        preventDuplicate: true,
+      });
+    },
+  });
+};
+
+const createSignUpToken = (data: any) => {
+  const token = localStorage.getItem("token");
+  return axios.post(`http://127.0.0.1:8000/generate_token/`, data, {
+    headers: {
+      Authorization: `Token ${token}`,
+    },
+  });
+};
+
+export const useCreateSignUpToken = () => {
+  const { enqueueSnackbar } = useSnackbar();
+
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({ data }: { data: any }) => createSignUpToken(data),
+    onSuccess: () => {
+      enqueueSnackbar("Token criado!", {
+        variant: "success",
+        anchorOrigin: {
+          vertical: "top",
+          horizontal: "center",
+        },
+        autoHideDuration: 5000,
+        preventDuplicate: true,
+      });
+      queryClient.invalidateQueries({ queryKey: ["request-acount"] });
+    },
+    onError: () => {
+      enqueueSnackbar("Um erro ocorreu! Tente novamente.", {
+        variant: "error",
+        anchorOrigin: {
+          vertical: "top",
+          horizontal: "center",
+        },
+        autoHideDuration: 5000,
+        preventDuplicate: true,
+      });
+    },
+  });
+};
