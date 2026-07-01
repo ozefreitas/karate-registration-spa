173e5381589fde35f20490b26a257f52bc4666cd
ozefreitas <jpsfreitas12@gmail.com>
2026-07-01 17:50:14 +0100

    [CHANGE] disabled text color for N/A values

--- src/components/EventCards/EventAllRegistryPage.tsx
+++ src/components/EventCards/EventAllRegistryPage.tsx
@@ -138,9 +138,24 @@ export default function EventAllRegistryPage(
             );
             const disciplineTeams = discipline?.teams.map((teamInfo: any) => ({
               id: teamInfo.team.id,
-              athlete1: teamInfo.team.athlete1?.full_name ?? "N/A",
-              athlete2: teamInfo.team.athlete2?.full_name ?? "N/A",
-              athlete3: teamInfo.team.athlete3?.full_name ?? "N/A",
+              athlete1:
+                teamInfo.team.athlete1 === null ? (
+                  <Typography color="textDisabled">N/A</Typography>
+                ) : (
+                  teamInfo.team.athlete1?.full_name
+                ),
+              athlete2:
+                teamInfo.team.athlete2 === null ? (
+                  <Typography color="textDisabled">N/A</Typography>
+                ) : (
+                  teamInfo.team.athlete2?.full_name
+                ),
+              athlete3:
+                teamInfo.team.athlete3 === null ? (
+                  <Typography color="textDisabled">N/A</Typography>
+                ) : (
+                  teamInfo.team.athlete3?.full_name
+                ),
               gender: teamInfo.team.gender,
               club: teamInfo.team.club,
               category:
